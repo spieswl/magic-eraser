0f698b643748bf01c70ae5f84b2b48b1de3659f2
rikago <l.hutson@live.com>
2017-11-25 18:19:20 -0600

    tiling is finished, the only thing to add would be image blending. example images added

--- imagetiling.py
+++ imagetiling.py
@@ -3,139 +3,201 @@ import cv2
 from PIL import Image
 from matplotlib import pyplot as plt
 from random import randint
-'''
-cap = cv2.VideoCapture('/home/rikako/Documents/git_directories/classes/Computer Vision/project/Original.avi')
-
-framnum=0
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if ret==True:
-    	framnum+=1
-    	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    	if framnum<100:
-    		cv2.circle( gray,( 200, 200 ), 32, ( 0, 0, 255 ), -1, 8 )
-    	cv2.imshow('frame',gray)
-  		#imshow("Image",image);
-    	if cv2.waitKey(25) & 0xFF == ord('q'):
-       		break
-    else:
-    	break
-
-cap.release()
-cv2.destroyAllWindows()'''
 
 
+def createpatch (im,tilesize,framewidth,frameheight):
+	
+	#creates a matrix of the same size as the blacked out area
+	img = np.zeros((tilesize,tilesize,3), np.uint8)
+	#chooses a random number for the position in which the tile is coming from 
+	randompatchheight=randint(0,frameheight-tilesize)
+	randompatchwidth=  randint(0,framewidth-tilesize) 
 
-im=cv2.imread('frame.png')
-image_size=im.size
-#orig=Image.open('moon.bmp').convert('L')
+	check=True
+	#will generate the random tile if there are any pixels that are within the banked out box then it will re
+	#re generate the patch
+	while check==True:
 
-#will put a balack box over the words
-#					x      y
-cv2.rectangle( im,( 1300, 300 ), (200,600), ( 255, 255, 255 ), -1, 8 )
+		for i in range(tilesize):
+		    for j in range(tilesize):
+		        img[i, j] = im[ randompatchwidth+ i, randompatchheight+ j] 
 
-framewidth=im.shape[0]
-frameheight=im.shape[1]
-tilesize=100
-overlapwidth=50
+		i4,i5,i6= np.where(img==(255,255,255)) 
+		i7,i8,i9= np.where(img==(125,0,0)) 
 
-#finds the coordinates where the black box is 
-imgy,i2x,i3= np.where(im==(255,255,255))
-#print im.shape[1]
-#creates a matrix of the same size as the blacked out area
-img = np.zeros((tilesize,tilesize,3), np.uint8)
-#chooses a random number for the position in which the tile is coming from 
-randompatchheight=randint(0,frameheight-tilesize)
-randompatchwidth=  randint(0,framewidth-tilesize) 
-check=True
+		if len(i4) >0 or len(i7) >0 :
+			check=True
+			randompatchheight=randint(0,frameheight-tilesize)
+			randompatchwidth=  randint(0,framewidth-tilesize) 
+		else:
+			check=False
+	return img
 
-#will generate the random tile if there are any pixels that are within the banked out box then it will re
-#re generate the patch
-while check==True:
 
-	for i in range(tilesize):
-	    for j in range(tilesize):
-	        img[i, j] = im[ randompatchwidth+ i, randompatchheight+ j] 
 
-	i4,i5,i6= np.where(img==(255,255,255)) 
-	if len(i4) >0:
-		check=True
-		randompatchheight=randint(0,frameheight-tilesize)
-		randompatchwidth=  randint(0,framewidth-tilesize) 
-	else:
-		check=False
 
-#creating th ssd
-th=0
-ar=[]
-ov=[]
-q=0
-diffcolor=np.zeros((3))
-print framewidth
-print frameheight
-for i in range(framewidth):
-	for j in range(frameheight):
-		#ovrow.append(im[ i, j])
-		if  im[ i, j][0] == 255 and th==0:
-			th=1
+
+def createssd (th,im,img,framewidth,frameheight,overlapwidth,tilesize):
+	#creating th ssd
+	
+	ar=[]
+	ov=[]
+	q=0
+	q2=0
+	
+	diffcolory=np.zeros((3))
+	diffcolorx=np.zeros((3))
+
+	#will check the ssd of the pixels on the right edge of the space to fill in
+	for i in range(framewidth):
+		for j in range(frameheight):
 			
+			if  im[ i, j][0] == 255 :
+				
+				
 
-			for x in range(i-(i-tilesize)):
-				row=[]
-				ovrow=[]
-				for y in range((j+overlapwidth)-j):
+				for x in range(i-(i-tilesize)):
+					#row=[]
+					#ovrow=[]
+					for y in range((j+overlapwidth)-j):
+						
+						
+						diffcolory[0]= (im[ i-x, j+y][0]-img[x,y])[0]
+						diffcolory[1]= (im[ i-x, j+y][1]-img[x,y])[1]
+						diffcolory[2]= (im[ i-x, j+y][2]-img[x,y])[2]
+						q+=(diffcolory[0]**2+diffcolory[1]**2+diffcolory[2]**2)**0.5
+						
+						t= (im[ i-x, j+y]-img[x,y])
+						#print img[x,y],im[ i-x, j+y],t,q
+
+						
+						#row.append((diffcolory[0]**2,diffcolory[1]**2,diffcolory[2]**2)) 
+						#ovrow.append(t)
+					#ar.append(row)
+					#ov.append(ovrow)
+				
+				#will check the ssd of the pixels on the upper edge of the space to fill in
+
+				for x in range((j+overlapwidth)-j):
+					#row=[]
+					#ovrow=[]
+					for y in range(i-(i-tilesize)):
+						diffcolorx[0]= (im[ i+x, j-y][0]-img[x,y])[0]
+						diffcolorx[1]= (im[ i+x, j-y][1]-img[x,y])[1]
+						diffcolorx[2]= (im[ i+x, j-y][2]-img[x,y])[2]
+
+						tr= (im[ i-x, j+y]-img[x,y])
+						q2+=(diffcolorx[0]**2+diffcolorx[1]**2+diffcolorx[2]**2)**0.5
+						#print q2
+						#row.append((diffcolorx[0]**2,diffcolorx[1]**2,diffcolorx[2]**2)) 
+						#ovrow.append(tr)
+					#ar.append(row)
+					#ov.append(ovrow)
 					
-					t= (im[ i-x, j+y]-img[x,y])
-					diffcolor[0]= (im[ i-x, j+y][0]-img[x,y])[0]
-					diffcolor[1]= (im[ i-x, j+y][1]-img[x,y])[1]
-					diffcolor[2]= (im[ i-x, j+y][2]-img[x,y])[2]
-					q+=(diffcolor[0]**2+diffcolor[1]**2+diffcolor[2]**2)**0.5
-					print img[x,y],im[ i-x, j+y],t,q
-
 					
-					row.append((diffcolor[0]**2,diffcolor[1]**2,diffcolor[2]**2)) 
-					ovrow.append(t)
-				ar.append(row)
-				ov.append(ovrow)
-			
-			if q<1000000:
-				for x in range(tilesize):
-					row=[]
-					ovrow=[]
-					for y in range(tilesize):
-						im[ i+x, j+y]=img[x,y]
+				
 
-plt.imshow(im)
-#plt.imshow(ov)
-plt.show()
+				#plt.imshow(ar) #overlap area
+				#plt.show()
 
-plt.imshow(ov) #overlap area
-plt.show()
+				#plt.imshow(img) #sample
+				#plt.show()
 
-plt.imshow(img) #sample
-plt.show()
-'''		
-print len(ov), len(ov[0])
-print len(img), len(img[0])
-#plt.imshow(ar)
-plt.imshow(ov)
-plt.show()
-plt.imshow(img)
-plt.show()
+				
+				#if the ssd is in the appropriate range then the tile will be added
+
+				#choose q and q2 values between 1.2M to 1.5M these are the threshholds
+				#the lower threashhold will make the prgram run slower because it becomes more selective
+				#higher threashhold will make the program run faster because it is less selective
+
+				if q<1500000 and q2<1300000 :
+					print q2
+					tile (im,img,tilesize,i,j)
+					'''
+					plt.imshow(im)
+					plt.show()
+					
+					plt.imshow(ov) #overlap area
+					plt.show()'''
+
+					#plt.imshow(ar) #overlap area
+					#plt.show()
+				else:
+					print 0
+					img=createpatch (im,tilesize,framewidth,frameheight)
+					createssd (th,im,img,framewidth,frameheight,overlapwidth,tilesize)
+				
+				
+
+def tile (im,img,tilesize,i,j):
+	
+	for x in range(tilesize):
+		row=[]
+		ovrow=[]
+		for y in range(tilesize):
+			im[ i+x, j+y]=img[x,y]
+
+'''
+#im = the frame gotten from the video
+	- the area that you want to be tiled over needs to be of color (255,255,255)
+	- the pen and the graphical error at the bottom of the frames should be overlayed with the color 
+	(125,0,0) otherwise tile samples with portions of these can be used to fill in the blank area
+
+#framewidth= the width in pixels of the frame
+
+#frameheight=the height in the pixels of the frame
+
+#tilesize= the size of the tile added to the empty space
+	- i suggest a size of 100
+	-if the size is too large then when tiling happens there is a chance that the tile will exceede
+	the frame size
+	- if the size is too small then it will take a long time to run
+#overlapwidth= the amount of overlap between the frame and the tile when checking the similarity between the 
+neighboring fames
 
 
-plt.imshow(ar)
-plt.gray()
-plt.show()
 
 
 
+'''
+def processimage (im,framewidth,frameheight,tilesize,overlapwidth):
+	img=createpatch (im,tilesize,framewidth,frameheight)
+	createssd (0,im,img,framewidth,frameheight,overlapwidth,tilesize)
+	
+	plt.imshow(im)
+	plt.show()
 
 
-#cv2.imshow('frame',img)
-cv2.imshow('dst_rt', im)
-cv2.waitKey(1000)
-cv2.destroyAllWindows()
+'''
+def main():
+	im=cv2.imread('frame.png')
+	image_size=im.size
+
+	#will put a balack box over the words
+	#					x      y
+	cv2.rectangle( im,( 1200, 300 ), (200,600), ( 255, 255, 255 ), -1, 8 )
+	cv2.rectangle( im,( 925, 250 ), (800,0), ( 125, 0, 0 ), -1, 8 )
+	cv2.rectangle( im,( 0, 1300 ), (1400,950), ( 125, 0, 0 ), -1, 8 )
+
+	framewidth=im.shape[0]
+	frameheight=im.shape[1]
+	tilesize=100
+	overlapwidth=25
+	
+	
+	#processimage(im,framewidth,frameheight,tilesize,overlapwidth)
+	plt.imshow(im)
+	plt.show()
+	
+	#plt.imshow(ov) #overlap area
+	#plt.show()
+
+	#plt.imshow(img) #sample
+	#plt.show()
+	
+
+if __name__ == '__main__':
+	main()
 '''
 
-#cv2.imshow('frame',im)
+
